a856ac96c6cdbd63a5dcad43a6ef2acb4e12b164
MMhenna <mhenna.mehdi@gmail.com>
2021-09-12 21:36:03 +0100

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,5 +10,5 @@ def hello():
     """ function to return hbnb """
     return("Hello HBNB!")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
